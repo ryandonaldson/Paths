3244bcf81a945994905f31903cdb55ada3beb335
LegitModern <legitmodern.dev@gmail.com>
2017-09-24 01:47:25 -0400

    Add basic iPhone app setup.

---
+++ Paths/Paths/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Paths
+//
+//  Created by Ryan Donaldson on 9/24/17.
+//  Copyright © 2017 Ryan Donaldson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
